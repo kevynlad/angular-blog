e1c03a48c329c28f8342bb81d52f70b858f85720
Kevyn Ladeira <101347752+kevynlad@users.noreply.github.com>
2025-03-20 10:11:27 -0300

    angular-blog-decola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+
+
 import { RouterModule, Routes } from '@angular/router';
 import { ContentComponent } from './pages/content/content.component';
 import { HomeComponent } from './pages/home/home.component';
